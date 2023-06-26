e6a6a1da03f864a2f4afe057054206273da403f4
Laveena Maharshi <77873287+LaveenaMaharshi@users.noreply.github.com>
2023-06-26 15:44:58 +0530

    Sorting Numbers final Commit

---
+++ sortNumbers.java
@@ -0,0 +1,34 @@
+public class sortNumbers {
+    public static void BubbleSort(int arr[]) {
+        int num = arr.length;
+        int temp = 0;
+        for (int i = 0; i < num; i++) {
+            for (int j = 0; j < num - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 0, 2, 1, 2, 1 };
+
+        System.out.println("Before Sorting the Array is: ");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + "  ");
+        }
+        System.out.println();
+
+        BubbleSort(arr);
+
+        System.out.println("After Sorting the Array is: ");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + "  ");
+        }
+
+        System.out.println();
+    }
+}
